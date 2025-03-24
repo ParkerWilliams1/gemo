ba2ea6777d67bb8e4525194cece4ba6f561a7803
Parker Williams <parkerb.williams@calbaptist.edu>
2025-03-23 22:39:44 -0700

    VideoSDK Create/Join Meeting Functional, Crashes on Leave Call

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'screens/home_screen.dart';
 import 'screens/chat_home_screen.dart';
 import 'screens/sign_in_screen.dart';
 import 'screens/sign_up_screen.dart';
+import 'video_stream/join_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,13 +27,15 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: SplashScreen.routeName,
+      // initialRoute: SplashScreen.routeName,
+      initialRoute: JoinScreen.routeName,
       routes: {
         SplashScreen.routeName: (context) => const SplashScreen(),
         HomeScreen.routeName: (context) => const HomeScreen(),
         ChatHomeScreen.routeName: (context) => ChatHomeScreen(),
         SignInScreen.routeName: (context) => const SignInScreen(),
         SignUpScreen.routeName: (context) => const SignUpScreen(),
+        JoinScreen.routeName: (context) => JoinScreen(),
       },
     );
   }
