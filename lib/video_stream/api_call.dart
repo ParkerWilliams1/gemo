c0973a38563d07b946a243178600bde609c182e9
Parker Williams <parkerb.williams@calbaptist.edu>
2025-03-26 19:12:50 -0700

    Updated Meeting Screen Buttons UI

--- lib/video_stream/api_call.dart
+++ lib/video_stream/api_call.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 //Auth token we will use to generate a meeting and connect to it
-String token = "YOUR_API_HERE";
+String token = "YOUR_API_GOES_HERE";
 
 // API call to create meeting
 Future<String> createMeeting() async {
--- lib/video_stream/meeting_controls.dart
+++ lib/video_stream/meeting_controls.dart
@@ -13,17 +13,66 @@ class MeetingControls extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ElevatedButton(
-            onPressed: onLeaveButtonPressed, child: const Text('Leave')),
-        ElevatedButton(
-            onPressed: onToggleMicButtonPressed, child: const Text('Toggle Mic')),
-        ElevatedButton(
+    return Positioned(
+      bottom: 30,
+      left: 0,
+      right: 0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Leave Button (leftmost)
+          _buildControlButton(
+            icon: Icons.call_end,
+            color: Color(0xFFFF8A8A), // Pastel red
+            onPressed: onLeaveButtonPressed,
+          ),
+          
+          SizedBox(width: 20),
+          
+          // Toggle Mic (center)
+          _buildControlButton(
+            icon: Icons.mic,
+            color: Color(0xFF83B9FF), // Pastel blue
+            onPressed: onToggleMicButtonPressed,
+          ),
+          
+          SizedBox(width: 20),
+          
+          // Toggle Camera (rightmost)
+          _buildControlButton(
+            icon: Icons.videocam,
+            color: Color(0xFFA5D6A7), // Pastel green
             onPressed: onToggleCameraButtonPressed,
-            child: const Text('Toggle WebCam')),
-      ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildControlButton({
+    required IconData icon,
+    required Color color,
+    required VoidCallback onPressed,
+  }) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: IconButton(
+        icon: Icon(icon, size: 28),
+        color: Colors.white,
+        onPressed: onPressed,
+      ),
     );
   }
 }
