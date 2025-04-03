b754135667108a69e9a684bea968c2301916a497
ramarca-cbu-egr302-e-admin <richard2marca@gmail.com>
2025-04-02 22:33:22 -0700

    fixed the gradle

--- lib/video_stream/api_call.dart
+++ lib/video_stream/api_call.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 //Auth token we will use to generate a meeting and connect to it
-String token = "YOUR_API_GOES_HERE";
+String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiIxMmUzZGIwZi0wZWYwLTQyMzMtODk2NC00YzJiZmY5ZDdkNzAiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTc0MzAzOTA1OCwiZXhwIjoxNzQzNjQzODU4fQ.brDMYGM0eiaFkCvnua89t5P8hR4fBIWu0lqQeekVbu8";
 
 // API call to create meeting
 Future<String> createMeeting() async {
